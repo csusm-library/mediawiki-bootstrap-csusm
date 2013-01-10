f9302b0023a2ac154897e9a20992878e035e1e65
Ian Chan <ichan@csusm.edu>
2013-01-10 15:55:44 -0800

    jQuery to add dropdown menu classes for left menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,6 @@
 var url = document.location.toString();
 jQuery(document).ready(function(){
+  jQuery('#leftnav ul').addClass("dropdown-menu").removeClass('nav-tabs');
   jQuery(".item-infolink").html("dbinfo");
   jQuery(".item-infolink").attr('title', 'Show database descriptions');
   jQuery(".item-infolink").live('click',function(){
@@ -16,20 +17,4 @@ jQuery(document).ready(function(){
     _gaq.push(['_trackPageview', outbound]);
     location.href = $(this).attr("href");
   });
-  if ($('#sidebar-left object').length != 0) {
-    if ($('.meebonote').length == 0) {
-        $('#sidebar-left object').wrap("<div class='meebonote'></div>");
-        $('.meebonote').attr({title: "If the librarian is offline, please include your email address. Thanks!"});
-      if ($('#meebo_notice').length == 0) {
-          $(".meebonote").append("<div id='meebo_notice'>If the librarian is offline, please include your email address. Thanks!</div>");
-        }
-    }
-     $(".meebonote").hover(function(e){
-        $("#meebo_notice").stop(true, true).animate({opacity: "show", top: '40px',right: "-170"}, "fast");
-    },
-    function(){
-      $("#meebo_notice").fadeOut('2500');
-    });
-    $('.menu_closer').click(function(){$('.jquery-float-menu2 ul').fadeOut('2000');});
-  }
 });
